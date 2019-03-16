ffe3de4e86a7d36ff4ab86e086fb3a76c147d52c
Steven Levine <slevine111@gmail.com>
2019-03-15 22:55:41 -0400

    created seed file

---
+++ bin/seed.js
@@ -0,0 +1,20 @@
+const dbInit = require('../server/DataAccess/index')
+const Product = require('../server/DataAccess/models/index')
+
+const syncAndSeed = () => {
+  return dbInit(true)
+    .then(() => {
+      return Product.create({
+        Name: 'Foo',
+        Price: 100.056,
+        Availability: 'instock'
+      })
+    })
+    .then(() => console.log('data successfully seeded'))
+    .catch(err => {
+      console.log('data not seeded. error below:')
+      console.log(err)
+    })
+}
+
+syncAndSeed()
