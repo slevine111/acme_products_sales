cabf267d86d47a21033c37e2c6fcadfe76281f0d
Steven Levine <slevine111@gmail.com>
2019-03-18 08:58:58 -0400

    Can;t get jest to work front-end

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ const path = require('path')
 module.exports = {
   collectCoverage: true,
   coverageDirectory: path.join(__dirname, 'TestCoverageReport'),
-  coverageReporters: ['text', 'html']
+  coverageReporters: ['text', 'html'],
+  verbose: true
 }
